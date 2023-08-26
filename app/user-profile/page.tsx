b3f8f3bbb2004ca528b729b5755ddff3e6a3bc68
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-08-26 03:55:15 -0700

    Add build plan comments.

--- app/user-profile/page.tsx
+++ app/user-profile/page.tsx
@@ -1,12 +1,24 @@
-'use client';
+'use client'
 
 import { useUser } from '@clerk/clerk-react'
 
 const UserProfilePage = () => {
   const { user } = useUser()
-  console.log('user: ', user);
+  console.log('user: ', user)
 
-  return <div className='relative'></div>
+  /*
+    * User Profile UI Elements
+    # User Info: Should show user's: name, email, team name (if on team).
+    # Action Buttons:
+    - If not on team: Should show buttons: Join Team, Create Team
+      > Each of these opens a dropdown with a list of joinable teams (not full).
+    - If on team: Should show buttons: Add Member, Leave Team
+      > Add member shows a dropdown with a list of unattached participants.
+
+    # If you think of anything else, feel free to add!
+  */
+
+  return <div className='relative'>User profile page!</div>
 }
 
 export default UserProfilePage
