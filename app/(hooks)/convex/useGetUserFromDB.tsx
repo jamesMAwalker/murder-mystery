cc5eec2d49bb1a7551115846bdc586aa432ead4b
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-08-27 10:02:27 -0700

    Add hook to abstract get user process.

---
+++ app/(hooks)/convex/useGetUserFromDB.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useEffect, useReducer, useState } from "react";
+import { useUser } from "@clerk/nextjs";
+import axios from "axios";
+
+const getUserDataFromDB = async (user_id: string) => {
+  return await axios.post('/api/getUser', { user_id })
+}
+
+export const useGetUserFromDB = () => {
+  const [user, setUser] = useState<IConvexUser>(Object)
+  const { user: clerkUser } = useUser()
+
+  useEffect(() => {
+    {
+      ;(async () => {
+        if (clerkUser?.id) {
+          try {
+            const res = await getUserDataFromDB(clerkUser.id)
+            const foundConvexUser = res.data.user
+
+            if (!foundConvexUser) throw Error('Could not find user!')
+
+            setUser(foundConvexUser)
+          } catch (error) {
+            console.error('error: ', error);
+          }
+        }
+      })()
+    }
+  }, [clerkUser])
+
+  return user;
+}
