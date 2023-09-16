e21493d812f807bb19002c643b7e31c93361109f
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-09-16 09:38:20 -0700

    Add hook for handling injection into context.

---
+++ app/(hooks)/convex/useGetAllUsersFromDB.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+const getUsersFromDB = async () => {
+  return await axios.get('/api/getUser')
+}
+
+export const useGetAllUsersFromDB = () => {
+  const [users, setUsers] = useState<IConvexUser[]>(Object)
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const res = await getUsersFromDB()
+
+        const foundConvexUsers = res.data.users
+
+        if (!foundConvexUsers) throw Error('Error fetching users!')
+
+        setUsers(foundConvexUsers)
+      } catch (error) {}
+    })()
+  }, [])
+
+  return users
+}
