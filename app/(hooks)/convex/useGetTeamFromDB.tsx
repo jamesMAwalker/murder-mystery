f1b8fa5fbc909ba637688eafb54eda38b67d86cd
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-09-16 07:47:46 -0700

    Add hook for accessing user data from db.

---
+++ app/(hooks)/convex/useGetTeamFromDB.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+const getTeamFromDB = async (team_id: string) => {
+  return await axios.post('/api/getTeam', { team_id: team_id })
+}
+
+export const useGetTeamFromDB = (team_id: string | null) => {
+  const [team, setTeam] = useState<IConvexTeam>(Object)
+
+  useEffect(() => {
+    if (team_id) {
+      ;(async () => {
+        try {
+          const res = await getTeamFromDB(team_id)
+          console.log('res: ', res)
+          const foundConvexTeam = res.data.team
+
+          if (!foundConvexTeam) throw Error('Could not find team!')
+
+          setTeam(foundConvexTeam)
+        } catch (error) {
+          console.error('error: ', error)
+        }
+      })()
+    }
+  }, [team_id])
+
+  return team
+}
