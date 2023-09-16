06b417585b22e8ab3308b18d297966719298c53b
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-09-16 09:39:53 -0700

    Add suspect placeholder data; type.

---
+++ app/suspects/data.ts
@@ -0,0 +1,86 @@
+export const SUSPECTS_DATA = {
+  heading: 'Suspects List',
+  list: [
+    {
+      id: '1',
+      name: 'Test',
+      desc: 'You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.' ,
+      image: {
+        src: '/placeholder.jpg',
+        alt: 'test'
+      },
+    },
+    {
+      id: '2',
+      name: 'Test',
+      desc: 'You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.' ,
+      image: {
+        src: '/placeholder.jpg',
+        alt: 'test'
+      },
+    },
+    {
+      id: '3',
+      name: 'Test',
+      desc: 'You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.' ,
+      image: {
+        src: '/placeholder.jpg',
+        alt: 'test'
+      },
+    },
+    {
+      id: '4',
+      name: 'Test',
+      desc: 'You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.' ,
+      image: {
+        src: '/placeholder.jpg',
+        alt: 'test'
+      },
+    },
+    {
+      id: '5',
+      name: 'Test',
+      desc: 'You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.' ,
+      image: {
+        src: '/placeholder.jpg',
+        alt: 'test'
+      },
+    },
+    {
+      id: '6',
+      name: 'Test',
+      desc: 'You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.' ,
+      image: {
+        src: '/placeholder.jpg',
+        alt: 'test'
+      },
+    },
+    {
+      id: '7',
+      name: 'Test',
+      desc: 'You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.' ,
+      image: {
+        src: '/placeholder.jpg',
+        alt: 'test'
+      },
+    },
+    {
+      id: '8',
+      name: 'Test',
+      desc: 'You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.' ,
+      image: {
+        src: '/placeholder.jpg',
+        alt: 'test'
+      },
+    },
+    {
+      id: '9',
+      name: 'Test',
+      desc: 'You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.' ,
+      image: {
+        src: '/placeholder.jpg',
+        alt: 'test'
+      },
+    },
+  ]
+}
