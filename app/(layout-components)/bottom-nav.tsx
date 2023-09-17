84ad0bd2b025c6a7f18a244dda4f0db6a69312ba
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-09-17 05:02:08 -0700

    Remove test code.

--- app/(layout-components)/bottom-nav.tsx
+++ app/(layout-components)/bottom-nav.tsx
@@ -7,14 +7,10 @@
   # In light of this, should we instead make this a slider rather than a dropdown?
 */
 
-import { useRef, useState, SyntheticEvent } from 'react'
+import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 
 import { cn } from '@/lib/utils'
-import { useGameContext } from '../(context)/game.context'
-import { useCreateTeamInDB } from '../(hooks)/convex/teams/useCreateTeamInDB'
-import { useUserContext } from '../(context)/user.context'
-import { useCreateTeamRequestInDB } from '../(hooks)/convex/requests/useCreateRequest'
 
 interface IDropdownContent {
   [key: string]: string
@@ -30,24 +26,6 @@ const pages: IDropdownContent[] = [
 ]
 
 export const BottomNav = () => {
-  const { user } = useUserContext()
-  const { createTeam } = useCreateTeamInDB()
-
-  const { createRequest } = useCreateTeamRequestInDB()
-
-  const handleTestClick = () => {
-    const team_id = '4gmbygg691879fkmnze3pxy89j5pt50'
-    const user_id = '498ncp61be3rq103xh9h80p49j5wcx8'
-    createRequest(team_id, user_id)
-  }
-  // const handleTestClick = (team_name: string) => {
-  //   if (user?._id) {
-  //     console.log('user: ', user);
-  //     createTeam(team_name, user._id)
-
-  //   }
-  // }
-
   const { push } = useRouter()
 
   const [current, setCurrent] = useState(0)
@@ -61,43 +39,30 @@ export const BottomNav = () => {
   }
 
   return (
-    <>
-      <button
-        // onClick={() => handleTestClick('FDJ')}
-        onClick={() => handleTestClick()}
-        className='fixed bottom-24 btn btn-accent'
-      >
-        Create Request
-      </button>
-      <div className='fixed bottom-0 left-0 rounded-none collapse bg-primary w-full'>
-        <input
-          onClick={() => setOpen(!open)}
-          type='checkbox'
-          className='peer'
-        />
-        <p className='collapse-title font-bold flex items-center justify-between bg-primary peer-checked:text-secondary-content'>
-          <span>{pages[current].page}</span>
-          <span className={cn(rotated)}>▲</span>
-        </p>
-        <ul className='w-full collapse-content flex-col-center p-0'>
-          {pages.map(({ page, path }, idx) => {
-            const active = idx === current
+    <div className='fixed bottom-0 left-0 rounded-none collapse bg-primary w-full'>
+      <input onClick={() => setOpen(!open)} type='checkbox' className='peer' />
+      <p className='collapse-title font-bold flex items-center justify-between bg-primary peer-checked:text-secondary-content'>
+        <span>{pages[current].page}</span>
+        <span className={cn(rotated)}>▲</span>
+      </p>
+      <ul className='w-full collapse-content flex-col-center p-0'>
+        {pages.map(({ page, path }, idx) => {
+          const active = idx === current
 
-            if (!active)
-              return (
-                <li
-                  className={cn(
-                    'w-full px-4 py-2 text-primary-content peer-checked:text-secondary-content'
-                  )}
-                  onClick={() => handleClick(idx, path)}
-                  key={page}
-                >
-                  {page}
-                </li>
-              )
-          })}
-        </ul>
-      </div>
-    </>
+          if (!active)
+            return (
+              <li
+                className={cn(
+                  'w-full px-4 py-2 text-primary-content peer-checked:text-secondary-content'
+                )}
+                onClick={() => handleClick(idx, path)}
+                key={page}
+              >
+                {page}
+              </li>
+            )
+        })}
+      </ul>
+    </div>
   )
 }
