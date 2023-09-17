7815a35688e3532f8b2573396aa7be2328746f1a
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-09-17 12:36:03 -0700

    Add API route for removing members.

---
+++ app/api/team/remove/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse, NextRequest } from "next/server";
+import { ConvexHttpClient } from "convex/browser";
+import { api } from "@/convex/_generated/api";
+
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
+
+export async function PATCH(req: NextRequest) {
+  const { team_id, user_id } = await req.json()
+
+  try {
+    const updatedTeam = await convex.mutation(api.teams.removeMember, { team_id, user_id })
+
+    if (!updatedTeam) {
+      return NextResponse.json({
+        status: 422,
+        message: 'Error updating team!'
+      })
+    }
+
+    return NextResponse.json({
+      status: 200,
+      message: 'Team updated!',
+      team: updatedTeam
+    })
+
+  } catch (error) {
+    console.error('error: ', error);
+
+    return NextResponse.json({
+      status: 500,
+      message: 'Something went wrong!',
+      error
+    })
+  }
+
+}
