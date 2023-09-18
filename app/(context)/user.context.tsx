f3a2e70a569201c3cd1c6ac01c33ac0b8b2f2e1f
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-09-18 01:23:51 -0700

    Add user invitations to user context.

--- app/(context)/user.context.tsx
+++ app/(context)/user.context.tsx
@@ -9,6 +9,7 @@ import {
 import { useGetUserFromDB } from '../(hooks)/convex/users/useGetUserFromDB'
 import { useGetTeamFromDB } from '../(hooks)/convex/teams/useGetTeamFromDB'
 import { useGetRequestsByUserID } from '../(hooks)/convex/requests/useGetRequestsByUserID'
+import { useGetInvitationsByUserID } from '../(hooks)/convex/invitations/useGetInvitationsByUserID'
 
 const defaults = {
   user: null
@@ -18,6 +19,7 @@ const UserContext = createContext<{
   user: IConvexUser | null
   team?: IConvexTeam
   requests?: IConvexRequest | null
+  invitations?: IConvexInvitation | null
 } | null>(defaults)
 
 export const useUserContext = () => useContext(UserContext)!
@@ -25,9 +27,11 @@ export const useUserContext = () => useContext(UserContext)!
 export const UserProvider = ({ children }: { children: ReactNode }) => {
   const user = useGetUserFromDB() || null
   const team = useGetTeamFromDB(user?.team_id) || null
+
   const requests = useGetRequestsByUserID(user?._id) || null
+  const invitations = useGetInvitationsByUserID(user?._id) || null
 
   return (
-    <UserContext.Provider value={{ user, team, requests }}>{children}</UserContext.Provider>
+    <UserContext.Provider value={{ user, team, requests, invitations }}>{children}</UserContext.Provider>
   )
 }
