b922fd948c68361f4aee06d526348f02c955aa24
Joshua <joshsmayhew@gmail.com>
2023-09-20 19:30:44 +0700

    add undefined Type to useGetTeamFromDb

--- app/(hooks)/convex/teams/useGetTeamFromDB.tsx
+++ app/(hooks)/convex/teams/useGetTeamFromDB.tsx
@@ -1,36 +1,36 @@
-'use client'
+"use client";
 
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const getTeamFromDB = async (team_id: string) => {
-  return await axios.post('/api/team/get', { team_id: team_id })
-}
+  return await axios.post("/api/team/get", { team_id: team_id });
+};
 
-export const useGetTeamFromDB = (team_id: string | null) => {
-  const [team, setTeam] = useState<IConvexTeam>(Object)
-  const [error, setError] = useState<string | null>(null)
+export const useGetTeamFromDB = (team_id: string | null | undefined) => {
+  const [team, setTeam] = useState<IConvexTeam>(Object);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     if (team_id) {
-      ;(async () => {
+      (async () => {
         try {
-          const res = await getTeamFromDB(team_id)
+          const res = await getTeamFromDB(team_id);
 
-          // console.log('_____res from get team hook_____: ', res)
+          console.log("_____res from get team hook_____: ", res);
 
-          const foundConvexTeam = res.data.team
+          const foundConvexTeam = res.data.team;
 
-          if (!foundConvexTeam) throw Error('Could not find team!')
+          if (!foundConvexTeam) throw Error("Could not find team!");
 
-          setTeam(foundConvexTeam)
+          setTeam(foundConvexTeam);
         } catch (error: any) {
-          console.error('error: ', error)
-          setError(error?.message)
+          console.error("error: ", error);
+          setError(error?.message);
         }
-      })()
+      })();
     }
-  }, [team_id])
+  }, [team_id]);
 
-  return team
-}
+  return team;
+};
