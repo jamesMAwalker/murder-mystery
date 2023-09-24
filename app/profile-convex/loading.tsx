045d9ab78ee40d2916b839c7273fae10e19fa510
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-09-24 13:03:04 -0700

    Add: Basic loader.

---
+++ app/profile-convex/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className='flex-center full !h-[80vh]'>
+      <span className='loading loading-ring loading-lg scale-150'></span>
+    </div>
+  )
+}
