aa7894a61977fb6460d9cceec9946bb2d9953119
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-09-25 04:56:50 -0700

    Update: Create fn error handling; get user from session.

--- convex/users.ts
+++ convex/users.ts
@@ -1,5 +1,6 @@
 import { query, mutation } from "./_generated/server";
 import { v } from "convex/values";
+import { getUserFromAuthSession } from "./lib/getUserFromAuthSession";
 
 export const create = mutation({
   args: {
@@ -7,17 +8,23 @@ export const create = mutation({
     name: v.string(),
     email: v.string(),
     has_team: v.boolean(),
+    is_captain: v.boolean(),
     team_id: v.null(),
     team_name: v.null(),
   },
   handler: async ({ db }, args) => {
+    
     // check if email exists in db.
     const existingUser = await db
       .query("users")
       .filter((user) => user.eq(user.field("email"), args.email))
       .unique();
 
-    if (existingUser) throw Error("Email already in use!");
+    // if (existingUser) throw Error("Email already in use!");
+    if (existingUser) {
+      return { message: 'Existing user found.', user: existingUser }
+    }
+
 
     // check if id exists in db.
     const existingId = await db
@@ -39,6 +46,21 @@ export const create = mutation({
 export const getAll = query(async ({ db }) => {
   return await db.query("users").collect();
 });
+
+export const getFromSession = query({
+  handler: async (ctx) => {
+
+    try {
+      // get and return user from session.
+      return await getUserFromAuthSession(ctx)
+      
+    } catch (error: any) {
+
+      // throw error if not found.
+      throw new Error('User not found.')
+    }
+  }
+})
 
 export const getById = query({
   args: { user_id: v.string() },
