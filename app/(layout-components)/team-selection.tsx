8612bd002fc15123a1db16d386ed8d83054e2998
Joshua <joshsmayhew@gmail.com>
2023-09-25 21:18:02 +0700

    change team buttons to team-selection and refactor to account for new convex functions

---
+++ app/(layout-components)/team-selection.tsx
@@ -0,0 +1,65 @@
+import { useUserContext } from "../(context)/user.context";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react-internal";
+import React from "react";
+
+interface TeamSelectionProps {
+  showJoinModal: () => void;
+  showCreateModal: () => void;
+}
+
+export const TeamSelection: React.FC<TeamSelectionProps> = ({
+  showJoinModal,
+  showCreateModal,
+}) => {
+  const user = useQuery(api.users.getFromSession);
+
+  const hasTeam = user?.has_team;
+  const team = user?.team_name;
+
+  if (hasTeam === null || hasTeam === undefined) {
+    return (
+      <div className="flex flex-col gap-4 w-full">
+        <h2 className="text-white text-lg font-semibold">Loading...</h2>
+      </div>
+    );
+  }
+
+  if (hasTeam) {
+    return (
+      <div className="flex flex-col gap-4 w-full">
+        <h2 className="text-white text-lg font-semibold">Team: {team}</h2>
+        <div className="flex flex-col sm:flex-row gap-4">
+          <button className="btn btn-primary w-full sm:w-auto">
+            Add Member
+          </button>
+          <button className="btn btn-accent w-full sm:w-auto">
+            Leave Team
+          </button>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex flex-col gap-4 w-full">
+        <h2 className="text-white text-lg font-semibold">
+          You are not on a team. Join or create one to get started!
+        </h2>
+        <div className="flex flex-col sm:flex-row gap-4">
+          <button
+            className="btn btn-primary cursor-pointer w-full sm:w-auto"
+            onClick={() => showJoinModal()}
+          >
+            Join Team
+          </button>
+          <button
+            className="btn btn-accent w-full sm:w-auto"
+            onClick={() => showCreateModal()}
+          >
+            Create Team
+          </button>
+        </div>
+      </div>
+    );
+  }
+};
