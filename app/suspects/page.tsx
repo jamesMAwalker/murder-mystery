fa853c7aebc57e7487fd4b7d00ea8a3786e803ab
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-09-25 14:24:22 -0700

    Update: Suspects page with new UI.

--- app/suspects/page.tsx
+++ app/suspects/page.tsx
@@ -1,68 +1,81 @@
-'use client';
+// @ts-nocheck
+'use client'
 
-import { useState } from 'react'
+import { useRef, useState } from 'react'
+import { useQuery } from 'convex/react-internal'
 
 import { SUSPECTS_DATA } from './data'
 import { ISuspectCard } from './types'
-
+import { api } from '@/convex/_generated/api'
 
 const SuspectsPage = () => {
-  const { heading, list } = SUSPECTS_DATA
+  const modalRef = useRef(null)
+  const { heading } = SUSPECTS_DATA
 
   const [modalContent, setModalContent] = useState<ISuspectCard | null>(null)
 
   const handleSuspectClick = (idx: number) => {
-    setModalContent(list[idx])
-    window.test_modal.showModal()
+    setModalContent(suspects[idx])
+    modalRef.current.showModal()
   }
 
+  const handleCloseModal = () => {
+    modalRef.current.close()
+  }
+
+  const suspects = useQuery(api.suspects.getAll)
 
   return (
     <div className='full flex-col-tl gap-8'>
       <h1 className='text-2xl uppercase mt-4'>{heading}</h1>
-      <ul className='full grid grid-cols-2 auto-rows-[300px] gap-[15px]'>
-        {list.map((item: ISuspectCard, idx: number) => {
+      <ul className='SUSPECT_LIST z-10 full grid grid-cols-2 auto-rows-auto gap-[15px]'>
+        {suspects?.map((suspect: any, idx: number) => {
           return (
-            <li key={item.id} className='full flex-col-bl gap-4'>
-              <div className='full relative'>
+            <li
+              key={suspect._id}
+              className='SUSPECT_ITEM full flex-col-bl'
+              onClick={() => handleSuspectClick(idx)}
+            >
+              <div className='w-full aspect-square relative'>
                 <img
-                  className='absolute full object-cover'
-                  src={item.image.src}
-                  alt={item.image.alt}
+                  className='absolute rounded-t-md full object-cover'
+                  src={suspect.image_url}
+                  alt={suspect.suspect_name}
                 />
               </div>
-              <button
-                onClick={() => handleSuspectClick(idx)}
-                className='btn w-full'
-              >
-                {item.name}
+              <button className='btn w-full rounded-t-none btn-secondary'>
+                {suspect.suspect_name}
               </button>
             </li>
           )
         })}
       </ul>
-      <dialog className='modal' id='test_modal'>
-        <form className='modal-box'>
-          <button className='btn btn-sm btn-circle btn-ghost absolute right-2 top-2'>
-            ✕
-          </button>
-          <div className='card pt-8 w-auto flex-col-tl gap-4'>
-            <figure>
-              <img
-                className='rounded-xl'
-                src={modalContent?.image.src}
-                alt={modalContent?.image.alt}
-              />
-            </figure>
-            <div className='card-body !p-0 w-full flex-col-tl gap-4'>
-              <h2 className='card-title'>{modalContent?.name} {modalContent?.id}</h2>
-              <p>{modalContent?.desc}</p>
-              <div className='card-actions w-full justify-end'>
-                <button className='btn btn-primary w-full'>Buy Now</button>
-              </div>
+      <dialog
+        className='SUSPECT_MODAL modal !top-[10%] !flex flex-col-center !h-[min-content] isolate'
+        ref={modalRef}
+      >
+        <button
+          onClick={handleCloseModal}
+          className='z-[1000] bg-white text-black btn btn-sm btn-circle btn-ghost absolute right-4 top-4'
+        >
+          ✕
+        </button>
+        <div className='card w-full glass'>
+          <figure>
+            <img
+              src={modalContent?.image_url}
+              alt={modalContent?.suspect_name}
+              className='object-cover aspect-square'
+            />
+          </figure>
+          <div className='card-body gap-4'>
+            <h2 className='card-title'>{modalContent?.suspect_name}</h2>
+            <p>{modalContent?.bio}</p>
+            <div className='card-actions justify-end'>
+              <button className='btn btn-primary'>Take Notes </button>
             </div>
           </div>
-        </form>
+        </div>
       </dialog>
     </div>
   )
