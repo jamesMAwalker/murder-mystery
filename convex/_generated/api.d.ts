0ef268462096a08b116a76d2cb19124fe68afaa9
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-09-25 14:22:27 -0700

    Update: Schema with new tables.

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -18,6 +18,7 @@ import type * as invitations from "../invitations";
 import type * as lib_getUserFromAuthSession from "../lib/getUserFromAuthSession";
 import type * as notes from "../notes";
 import type * as requests from "../requests";
+import type * as suspects from "../suspects";
 import type * as teams from "../teams";
 import type * as users from "../users";
 
@@ -34,6 +35,7 @@ declare const fullApi: ApiFromModules<{
   "lib/getUserFromAuthSession": typeof lib_getUserFromAuthSession;
   notes: typeof notes;
   requests: typeof requests;
+  suspects: typeof suspects;
   teams: typeof teams;
   users: typeof users;
 }>;
--- convex/schema.ts
+++ convex/schema.ts
@@ -26,4 +26,22 @@ export default defineSchema({
     requested_team_id: v.id('teams'),
     accepted: v.boolean()
   }),
+  suspects: defineTable({
+    age: v.string(),
+    bio: v.string(),
+    gender: v.string(),
+    home_country: v.string(),
+    image_url: v.union(v.string(), v.null()),
+    is_guilty: v.boolean(),
+    occupation: v.string(),
+    suspect_name: v.string(),
+    user_id: v.union(v.string(), v.null()),
+  }),
+  notes: defineTable({
+    note: v.string(),
+    suspect_id: v.id("suspects"),
+    suspect_name: v.string(),
+    user_id: v.id("users"),
+    user_name: v.string(),
+  }),
 });
