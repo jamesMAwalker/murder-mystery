5e7ccb6271272a0a8a83231d9f2f7930d44a6102
Joshua <joshsmayhew@gmail.com>
2023-09-25 21:47:48 +0700

    refactor create team modal to display in updated user profile

--- app/(layout-components)/create-team-modal.tsx
+++ app/(layout-components)/create-team-modal.tsx
@@ -1,55 +1,67 @@
 import React, { useState, useRef, useEffect } from "react";
 import { useCreateTeamInDB } from "../(hooks)/convex/teams/useCreateTeamInDB";
 import { useUserContext } from "../(context)/user.context";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react-internal";
 
-export const CreateTeamModal: React.FC = () => {
+interface CreateTeamModalProps {
+  closeModal: () => void;
+}
+
+export const CreateTeamModal: React.FC<CreateTeamModalProps> = ({
+  closeModal,
+}) => {
+  const user = useQuery(api.users.getFromSession);
+  const teams = useQuery(api.teams.getAll);
   const [teamName, setTeamName] = useState("");
+  console.log("teamName: ", teamName);
   const modalRef = useRef<HTMLInputElement | null>(null);
-  const { createTeam } = useCreateTeamInDB();
-  const { activeProfileModal, hideProfileModal, setLoggedUser, loggedUser } =
-    useUserContext();
 
-  useEffect(() => {
-    if (activeProfileModal === "create-team-modal") {
-      modalRef.current?.focus();
-    }
-  }, [activeProfileModal]);
+  // useEffect(() => {
+  //   if (activeProfileModal === "create-team-modal") {
+  //     modalRef.current?.focus();
+  //   }
+  // }, [activeProfileModal]);
 
   const handleTeamNameInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTeamName(e.target.value);
   };
 
-  const handleCreateTeamSubmit = async (
-    e: React.FormEvent<HTMLFormElement>
-  ) => {
-    e.preventDefault();
-    if (!loggedUser?._id) return;
-
-    createTeam(teamName, loggedUser._id);
-
-    const updatedUser = { ...loggedUser, has_team: true, team_name: teamName };
-
-    setLoggedUser(updatedUser);
-    setTeamName("");
-    hideProfileModal();
+  const handleCreateTeamSubmit = () => {
+    if (!user?._id) return;
   };
 
-  if (activeProfileModal !== "create-team-modal" || !loggedUser) {
+  // const handleCreateTeamSubmit = async (
+  //   e: React.FormEvent<HTMLFormElement>
+  // ) => {
+  //   e.preventDefault();
+  //   if (!user?._id) return;
+
+  //   createTeam(teamName, loggedUser._id);
+
+  //   const updatedUser = { ...loggedUser, has_team: true, team_name: teamName };
+
+  //   setLoggedUser(updatedUser);
+  //   setTeamName("");
+  //   hideProfileModal();
+  // };
+
+  if (!user) {
     return null;
   }
 
   return (
     <div className="flex flex-col items-center justify-start h-full bg-black !fixed w-screen h-screen z-10 inset-0 p-4">
-      {loggedUser && (
+      {user && (
         <div className="modal-box bg-gray-800 rounded-lg shadow-2xl p-6 w-full max-w-md mt-20">
           <button
             className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-            onClick={hideProfileModal}
+            onClick={closeModal}
           >
             ✕
           </button>
           <h3 className="font-bold text-xl mb-4">Create a Team</h3>
-          <form onSubmit={handleCreateTeamSubmit}>
+          <form>
             <div className="form-control w-full mb-4">
               <label className="label text-lg mb-2">
                 <span className="label-text">What is your team name?</span>
