08e66a8de953dca3b177d091cf9b882ebf877680
Joshua <joshsmayhew@gmail.com>
2023-09-25 21:48:04 +0700

    adding request functionality to join team modal

--- app/(layout-components)/join-team-modal.tsx
+++ app/(layout-components)/join-team-modal.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { useCreateRequestInDB } from "../(hooks)/convex/requests/useCreateRequest";
-import { useUserContext } from "../(context)/user.context";
+// import { useCreateRequestInDB } from "../(hooks)/convex/requests/useCreateRequest";
+// import { useUserContext } from "../(context)/user.context";
 import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react-internal";
 import { useGetRequestsByUserID } from "../(hooks)/convex/requests/useGetRequestsByUserID";
@@ -12,65 +12,55 @@ interface JoinTeamModalProps {
 export const JoinTeamModal: React.FC<JoinTeamModalProps> = ({ closeModal }) => {
   const user = useQuery(api.users.getFromSession);
   const teams = useQuery(api.teams.getAll);
-  console.log("teams: ", teams);
+  const userId = user?._id;
+  const createTeamRequest = () => {
+    console.log("createTeamRequest");
+    const request = api.users.createRequest(userId);
+  };
 
-  const userId = user?._id || undefined;
-  console.log("userId: ", userId);
-
-  const requests = useGetRequestsByUserID(userId);
-  console.log("requests: ", requests);
-
+  // Uncomment when needed
   // const { createRequest, request, error } = useCreateRequestInDB();
-
-  // const filteredTeamsByExistingRequest = teams?.filter((team: any) => {
-  //   if (Array.isArray(requests)) {
-  //     const hasNoExistingRequest = requests?.some(
-  //       (request) => request.requested_team_id !== team._id
-  //     );
-  //     return hasNoExistingRequest;
-  //   }
-  // });
-
-  // const teamsWithAvailableSpots = filteredTeamsByExistingRequest?.filter(
-  //   (team: any) => team.members.length <= 10
-  // );
+  // const requests = useGetRequestsByUserID(userId);
+  // const handleTeamRequest = (team_id: string, user_id: string) => {
+  //   createRequest(team_id, user_id);
+  //   closeModal();
+  // };
 
   if (!teams || !userId) {
     return null;
   }
 
-  // const handleTeamRequest = (team_id: string, user_id: string) => {
-  //   createRequest(team_id, user_id);
-  //   hideProfileModal();
-  // };
-
   return (
-    <div className="flex-col-tl bg-black gap-4 !fixed w-screen h-screen z-10 inset-0 p-4">
-      <button
-        className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-        onClick={closeModal}
-      >
-        &times;
-      </button>
-      <h3 className="font-bold text-lg">Available Teams</h3>
-      {/* {teams && teams.length > 0 && (
-        <ul className=" flex-col-tl gap-4 w-full overflow-scroll">
-          {teamsWithAvailableSpots?.map((team: any) => (
-            <li
-              key={team.team_name}
-              className="flex items-center justify-between p-4 bg-slate-800 gap-2 w-full rounded-md"
-            >
-              <span className="font-semibold">{team.team_name}</span>
-              <button
-                className="btn btn-primary"
-                onClick={() => handleTeamRequest(team._id, userId)}
+    <div className="flex flex-col items-center justify-top h-screen bg-black fixed w-screen inset-0 p-4 z-10">
+      <div className="modal-box bg-gray-800 rounded-lg shadow-2xl p-6 w-full max-w-md">
+        <button
+          className="btn btn-sm btn-circle btn-ghost absolute top-2 right-2 p-2"
+          onClick={closeModal}
+        >
+          &times;
+        </button>
+        <h3 className="font-bold text-xl mb-4">Available Teams</h3>
+
+        {teams.length > 0 && (
+          <ul className="flex-col gap-4 w-full overflow-y-auto">
+            {teams.map((team: any) => (
+              <li
+                key={team._id}
+                className="flex items-center justify-between p-4 bg-slate-800 gap-2 w-full rounded-md"
               >
-                Join
-              </button>
-            </li>
-          ))}
-        </ul>
-      )} */}
+                <span className="font-semibold text-lg">{team.team_name}</span>
+                <button
+                  className="btn btn-primary btn-md"
+                  // Uncomment when function is ready
+                  // onClick={() => handleTeamRequest(team._id, userId)}
+                >
+                  Join
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
