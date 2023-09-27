d58661c298c161fd9b4904484b54314d0f81612b
Joshua <joshsmayhew@gmail.com>
2023-09-27 22:34:45 +0700

    refactor and clean up add member modal

--- app/(layout-components)/add-member-modal.tsx
+++ app/(layout-components)/add-member-modal.tsx
@@ -10,29 +10,29 @@ export const AddMemberModal: React.FC<AddMemberModalProps> = ({
   closeModal,
 }) => {
   const user = useQuery(api.users.getFromSession);
-  console.log("user: ", user);
+
   const team = useQuery(api.teams.getFromSession);
   const teamId = team?._id;
 
   const allUsers = useQuery(api.users.getAll);
   const usersNotCurrentlyOnTeam = allUsers?.filter((user) => !user.has_team);
+  console.log("usersNotCurrentlyOnTeam: ", usersNotCurrentlyOnTeam);
 
   const invitePlayer = useMutation(api.invitations.create);
   const teamInvites = useQuery(api.invitations.getFromSessionByTeam);
+  console.log("teamInvites: ", teamInvites);
   const invitedUserIds = new Set(
     teamInvites?.map((invite: any) => invite.invited_user_id)
   );
-  const hasBeenInvited = usersNotCurrentlyOnTeam?.some((user) =>
-    invitedUserIds.has(user._id)
-  );
+  const hasBeenInvited = (user: any) => invitedUserIds.has(user._id);
+  console.log("invitedUserIds: ", invitedUserIds);
 
-  console.log("hasBeenInvited: ", hasBeenInvited);
   return (
     <div className="flex flex-col items-center justify-start h-full bg-black !fixed w-screen h-screen z-10 inset-0 p-4">
       {user && teamId && (
         <div className="modal-box bg-gray-800 rounded-lg shadow-2xl p-6 w-full max-w-md mt-20 relative">
           <button
-            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+            className="btn btn-sm btn-circle btn-ghost absolute top-2 right-2 p-2"
             onClick={closeModal}
           >
             &times;
@@ -41,36 +41,45 @@ export const AddMemberModal: React.FC<AddMemberModalProps> = ({
             Invite players to your team
           </h3>
 
-          <ul className="flex-col gap-2 sm:gap-4 w-full overflow-y-auto">
-            {usersNotCurrentlyOnTeam?.map((userToInvite: any) => (
-              <li
-                key={userToInvite._id}
-                className="flex items-center justify-between p-4 bg-slate-800 gap-2 w-full rounded-md"
-              >
-                <span className="font-bold text-lg">{userToInvite.name}</span>
-                {hasBeenInvited ? (
-                  <button
-                    className="btn btn-primary text-sm sm:text-base"
-                    disabled
+          {!teamInvites ? (
+            <div className="flex items-left justify-left w-full">
+              <div>Loading...</div>
+            </div>
+          ) : (
+            <ul className="flex-col gap-2 sm:gap-4 w-full overflow-y-auto">
+              {teamInvites &&
+                usersNotCurrentlyOnTeam?.map((userToInvite: any) => (
+                  <li
+                    key={userToInvite._id}
+                    className="flex items-center justify-between p-4 bg-slate-800 gap-2 w-full rounded-md"
                   >
-                    Invited
-                  </button>
-                ) : (
-                  <button
-                    className="btn btn-primary text-sm sm:text-base"
-                    onClick={() =>
-                      invitePlayer({
-                        team_id: teamId,
-                        user_id: userToInvite._id,
-                      })
-                    }
-                  >
-                    Invite
-                  </button>
-                )}
-              </li>
-            ))}
-          </ul>
+                    <span className="font-bold text-lg">
+                      {userToInvite.name}
+                    </span>
+                    {hasBeenInvited(userToInvite) ? (
+                      <button
+                        className="btn btn-primary text-sm sm:text-base"
+                        disabled
+                      >
+                        Invited
+                      </button>
+                    ) : (
+                      <button
+                        className="btn btn-primary text-sm sm:text-base"
+                        onClick={() =>
+                          invitePlayer({
+                            team_id: teamId,
+                            user_id: userToInvite._id,
+                          })
+                        }
+                      >
+                        Invite
+                      </button>
+                    )}
+                  </li>
+                ))}
+            </ul>
+          )}
         </div>
       )}
     </div>
