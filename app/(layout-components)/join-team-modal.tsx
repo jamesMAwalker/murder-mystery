28e792d322aad379b4170cbdec99a5c8f50b9fde
Joshua <joshsmayhew@gmail.com>
2023-09-27 22:35:29 +0700

    refactor join team modal implement loading

--- app/(layout-components)/join-team-modal.tsx
+++ app/(layout-components)/join-team-modal.tsx
@@ -6,57 +6,47 @@ interface JoinTeamModalProps {
   closeModal: () => void;
 }
 
+interface Team {
+  _id: string;
+  team_name: string;
+}
+
+interface Request {
+  requested_team_id: string;
+}
+
 export const JoinTeamModal: React.FC<JoinTeamModalProps> = ({ closeModal }) => {
   const user = useQuery(api.users.getFromSession);
   const teams = useQuery(api.teams.getAll);
   const requests = useQuery(api.requests.getFromSessionByUser);
-  const userId = user?._id;
-  const createRequest = useMutation(api.requests.createFromSession);
+  const requestedTeamIds =
+    requests?.map((request: Request) => request.requested_team_id) || [];
 
-  const renderJoinTeams = () => {
-    const requestedTeamIds = requests?.map(
-      (request: any) => request.requested_team_id
-    );
+  const createRequest = useMutation<any>(api.requests.createFromSession); // Add <any> or the expected mutation return type if needed
 
-    return (
-      <ul className="flex-col gap-4 w-full overflow-y-auto">
-        {teams?.map((team: any) => {
-          if (requestedTeamIds?.includes(team._id)) {
-            return (
-              <li
-                key={team._id}
-                className="flex items-center justify-between p-4 bg-slate-800 gap-2 w-full rounded-md"
-              >
-                <span className="font-semibold text-lg">{team.team_name}</span>
-                <button className="btn btn-secondary btn-md" disabled>
-                  Requested
-                </button>
-              </li>
-            );
-          } else {
-            return (
-              <li
-                key={team._id}
-                className="flex items-center justify-between p-4 bg-slate-800 gap-2 w-full rounded-md"
-              >
-                <span className="font-semibold text-lg">{team.team_name}</span>
-                <button
-                  className="btn btn-primary btn-md"
-                  onClick={() => createRequest({ team_id: team._id })}
-                >
-                  Request to Join
-                </button>
-              </li>
-            );
-          }
-        })}
-      </ul>
-    );
-  };
+  const hasBeenRequested = (teamId: string) =>
+    requestedTeamIds.includes(teamId);
 
-  if (!teams || !userId) {
-    return null;
-  }
+  const renderTeam = (team: Team) => (
+    <li
+      key={team._id}
+      className="flex items-center justify-between p-4 bg-slate-800 gap-2 w-full rounded-md"
+    >
+      <span className="font-semibold text-lg">{team.team_name}</span>
+      {hasBeenRequested(team._id) ? (
+        <button className="btn btn-secondary btn-md" disabled>
+          Requested
+        </button>
+      ) : (
+        <button
+          className="btn btn-primary btn-md"
+          onClick={() => createRequest({ team_id: team._id })}
+        >
+          Request to Join
+        </button>
+      )}
+    </li>
+  );
 
   return (
     <div className="flex flex-col items-center justify-start h-full bg-black !fixed w-screen h-screen z-10 inset-0 p-4">
@@ -68,8 +58,15 @@ export const JoinTeamModal: React.FC<JoinTeamModalProps> = ({ closeModal }) => {
           &times;
         </button>
         <h3 className="font-bold text-xl mb-4">Available Teams</h3>
-
-        {teams.length > 0 && renderJoinTeams()}
+        {teams && user && requests ? (
+          <ul className="flex-col gap-4 w-full overflow-y-auto">
+            {teams?.map(renderTeam)}
+          </ul>
+        ) : (
+          <div className="flex items-left justify-left w-full">
+            <div>Loading...</div>{" "}
+          </div>
+        )}
       </div>
     </div>
   );
