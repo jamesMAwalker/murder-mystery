5a0461a8ca9d49fa13fb868a9a4fb16ee9d1dd6b
Joshua <joshsmayhew@gmail.com>
2023-09-28 13:30:45 +0700

    refactor notifications-section for mobile first display

--- app/(layout-components)/notifications-section.tsx
+++ app/(layout-components)/notifications-section.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "convex/react-internal";
 import { api } from "@/convex/_generated/api";
 
@@ -6,22 +6,25 @@ export const NotificationsSection: React.FC = () => {
   const user = useQuery(api.users.getFromSession);
 
   return (
-    <div className="p-4 rounded-lg ">
-      {!user?.has_team && <PendingInvites />}
-      {user?.has_team && user?.is_captain && <RequestsToJoin />}
+    <div className="p-4 rounded-lg">
+      {!user?.has_team ? <PendingInvites /> : null}
+      {user?.has_team && user?.is_captain ? <RequestsToJoin /> : null}
     </div>
   );
 };
 
 const PendingInvites: React.FC = () => {
+  const [showAllInvitations, setShowAllInvitations] = useState(false);
   const pendingInvitations = useQuery(api.invitations.getFromSessionByUser);
-  console.log("pendingInvitations: ", pendingInvitations);
-
   const teams = useQuery(api.teams.getAll);
+
   const invitingTeams = teams?.filter((team) =>
     pendingInvitations?.some((invite) => invite.inviting_team_id === team._id)
   );
-  console.log("invitingTeams: ", invitingTeams);
+
+  const displayedInvitations = showAllInvitations
+    ? invitingTeams
+    : invitingTeams?.slice(0, 3);
 
   return (
     <div className="p-4 bg-slate-800 rounded-lg">
@@ -31,34 +34,47 @@ const PendingInvites: React.FC = () => {
           You do not have any pending invitations.
         </h2>
       ) : (
-        <InvitationList
-          invitations={pendingInvitations}
-          invitingTeams={invitingTeams}
-        />
+        <>
+          <InvitationList invitations={displayedInvitations} />
+          {!showAllInvitations && (
+            <button
+              className="btn btn-secondary"
+              onClick={() => setShowAllInvitations(true)}
+            >
+              Show More
+            </button>
+          )}
+        </>
       )}
     </div>
   );
 };
 
-const InvitationList: React.FC<{
-  invitations: any[];
-  invitingTeams: any[];
-}> = ({ invitations, invitingTeams }) => (
-  <ul className="flex-col gap-4 w-full overflow-y-auto">
-    {invitingTeams?.map((invite) => (
+const InvitationList: React.FC<{ invitations: any[] }> = ({ invitations }) => (
+  <ul className="flex flex-col gap-4 w-full overflow-y-auto">
+    {invitations?.map((invite) => (
       <li
         key={invite._id}
-        className="flex items-center justify-between p-4 bg-slate-800 gap-2 w-full rounded-md"
+        className="flex flex-col md:flex-row items-center justify-between p-4 bg-slate-800 gap-2 w-full rounded-md"
       >
-        <span className="font-semibold text-lg">Team: {invite.team_name}</span>
-        <button className="btn btn-primary">Accept</button>
-        <button className="btn btn-primary">Decline</button>
+        <span className="font-semibold text-lg md:mr-2">
+          Team: {invite.team_name}
+        </span>
+        <div className="flex gap-2 mt-2 md:mt-0">
+          <button className="btn btn-success" title="Accept this invitation">
+            Accept
+          </button>
+          <button className="btn btn-danger" title="Decline this invitation">
+            Decline
+          </button>
+        </div>
       </li>
     ))}
   </ul>
 );
 
 const RequestsToJoin: React.FC = () => {
+  const [showAllRequests, setShowAllRequests] = useState(false);
   const teamRequests = useQuery(api.requests.getFromSessionByTeam);
   const allPlayers = useQuery(api.users.getAll);
 
@@ -69,11 +85,13 @@ const RequestsToJoin: React.FC = () => {
   const requestingPlayers = allPlayers?.filter((player) =>
     RequestingPlayerIds.has(player._id)
   );
-  console.log("requestingPlayers: ", requestingPlayers);
 
-  console.log("RequestingPlayerIds: ", RequestingPlayerIds);
-  console.log("teamRequests: ", teamRequests);
+  const displayedRequests = showAllRequests
+    ? requestingPlayers
+    : requestingPlayers?.slice(0, 3);
+
   const team = useQuery(api.teams.getFromSession);
+
   return (
     <div className="p-4 bg-slate-800 rounded-lg">
       <h1 className="mb-2 font-bold text-white">Player Requests:</h1>
@@ -82,7 +100,17 @@ const RequestsToJoin: React.FC = () => {
           There are no requests to join {team?.team_name} at this time.
         </h2>
       ) : (
-        <RequestList requestingPlayers={requestingPlayers} />
+        <>
+          <RequestList requestingPlayers={displayedRequests} />
+          {!showAllRequests && (
+            <button
+              className="btn btn-secondary"
+              onClick={() => setShowAllRequests(true)}
+            >
+              Show More
+            </button>
+          )}
+        </>
       )}
     </div>
   );
@@ -91,16 +119,24 @@ const RequestsToJoin: React.FC = () => {
 const RequestList: React.FC<{ requestingPlayers: any[] }> = ({
   requestingPlayers,
 }) => (
-  <ul className="flex-col gap-4 w-full overflow-y-auto">
+  <ul className="flex flex-col gap-4 w-full overflow-y-auto">
     {requestingPlayers?.map((player) => (
       <li
         key={player._id}
-        className="flex items-center justify-between p-4 bg-slate-800 gap-2 w-full rounded-md"
+        className="flex flex-col md:flex-row items-center justify-between p-4 bg-slate-800 gap-2 w-full rounded-md"
       >
-        <span className="font-semibold text-lg">{player.name}</span>
+        <span className="font-semibold text-lg md:mr-2">{player.name}</span>
+        <div className="flex gap-2 mt-2 md:mt-0">
+          <button className="btn btn-success" title="Accept this player">
+            Accept
+          </button>
+          <button className="btn btn-danger" title="Decline this player">
+            Decline
+          </button>
+        </div>
       </li>
     ))}
-    <button className="btn btn-primary">Accept</button>
-    <button className="btn btn-primary">Decline</button>
   </ul>
 );
+
+export default NotificationsSection;
