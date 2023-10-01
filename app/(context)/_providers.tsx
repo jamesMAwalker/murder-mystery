b1cdb8bfc91a83cf927e52e751bda908c978768c
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-10-01 05:32:01 -0700

    Updates.

--- app/(context)/_providers.tsx
+++ app/(context)/_providers.tsx
@@ -1,15 +1,13 @@
 import { AccountProvider } from "./account.context";
 import { ClerkConvexProvider } from "./clerk-convex.context";
-import { GameProvider } from "./game.context";
+// import { GameProvider } from "./game.context";
 import { SuspectProvider } from "./suspect.context";
 // import { UserProvider } from './user.context'
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkConvexProvider>
-      <GameProvider>
         <SuspectProvider>{children}</SuspectProvider>
-      </GameProvider>
     </ClerkConvexProvider>
   );
 };
