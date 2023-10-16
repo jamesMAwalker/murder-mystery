9b86b54c093e2e4168606c764e546d675f95c55c
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-10-16 03:56:13 -0700

    Update: Replace rounds with phased rounds in admin page.

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,17 @@
+'use client';
+
+import { useState } from 'react'
+
+import { cn } from '@/lib/utils'
+
 import { ReportSection } from './reporting-section'
-import { RoundsSection } from './rounds-section'
+import { PhasedRoundsManager } from './phased-rounds-manager';
 
 const AdminDashboard = () => {
-  
+  const [activeTab, setActiveTab] = useState('Rounds')
+
   return (
-    <div className='flex-col-tl gap-24 w-full'>
+    <div className='flex-col-tl gap-10   w-full'>
       <div className='INTRO flex-col-tl gap-2 w-full'>
         <h1 className='text-2xl font-bold'>Admin Dashboard</h1>
         <p>
@@ -13,8 +20,23 @@ const AdminDashboard = () => {
           handled on this page.
         </p>
       </div>
-      <RoundsSection />
-      <ReportSection />
+      <div className='tabs tabs-boxed'>
+        {['Rounds', 'Reports'].map((tab) => {
+          const isActive = tab === activeTab
+
+          return (
+            <a
+              onClick={() => setActiveTab(tab)}
+              className={cn('tab tab-lg', isActive && 'tab-active text-accent')}
+              key={tab}
+            >
+              {tab}
+            </a>
+          )
+        })}
+      </div>
+      {activeTab === 'Rounds' && <PhasedRoundsManager />}
+      {activeTab === 'Reports' && <ReportSection />}
     </div>
   )
 }
