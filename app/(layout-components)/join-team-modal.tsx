c2e1125d297851721b8a5bd654de15a0d0d124a4
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-10-17 06:35:23 -0700

    Update: Refactor styling; formatting.

--- app/(layout-components)/join-team-modal.tsx
+++ app/(layout-components)/join-team-modal.tsx
@@ -1,79 +1,89 @@
-"use client";
+'use client'
 
-import React from "react";
-import { api } from "@/convex/_generated/api";
-import { useQuery, useMutation } from "convex/react-internal";
-import { ModalWrapper } from "./modal-wrapper";
+import React from 'react'
+import { api } from '@/convex/_generated/api'
+import { useQuery, useMutation } from 'convex/react-internal'
+import { ModalWrapper } from './modal-wrapper'
 
 interface JoinTeamModalProps {
-  closeModal: () => void;
+  closeModal: () => void
 }
 
 interface Team {
-  _id: string;
-  team_name: string;
+  _id: string
+  team_name: string
 }
 
 interface Request {
-  requested_team_id: string;
+  requested_team_id: string
 }
 
 export const JoinTeamModal: React.FC<JoinTeamModalProps> = ({ closeModal }) => {
-  const user = useQuery(api.users.getFromSession);
-  const teams = useQuery(api.teams.getAll);
-  const requests = useQuery(api.requests.getFromSessionByUser);
-  console.log("requests: ", requests);
+  const user = useQuery(api.users.getFromSession)
+  const teams = useQuery(api.teams.getAll)
+  const requests = useQuery(api.requests.getFromSessionByUser)
   const requestedTeamIds =
-    requests?.map((request: Request) => request.requested_team_id) || [];
+    requests?.map((request: Request) => request.requested_team_id) || []
 
-  const createRequest = useMutation<any>(api.requests.createFromSession); // Add <any> or the expected mutation return type if needed
+  const createRequest = useMutation<any>(api.requests.createFromSession) // Add <any> or the expected mutation return type if needed
 
-  const hasBeenRequested = (teamId: string) =>
-    requestedTeamIds.includes(teamId);
+  const hasBeenRequested = (teamId: string) => requestedTeamIds.includes(teamId)
 
   const renderTeam = (team: Team) => (
     <li
       key={team._id}
-      className="flex items-center justify-between p-4 bg-slate-800 gap-2 w-full rounded-md"
+      className='RENDER_TEAM flex-col-tl p-4 gap-4 w-full rounded-md bg-slate-700/30 p4'
     >
-      <span className="font-semibold text-lg">{team.team_name}</span>
-      {hasBeenRequested(team._id) ? (
-        <button className="btn btn-secondary btn-md" disabled>
-          Requested
-        </button>
-      ) : (
-        <button
-          className="btn btn-primary btn-md"
-          onClick={() => createRequest({ team_id: team._id })}
-        >
-          Request to Join
-        </button>
-      )}
+      <div className='font-semibold text-lg flex gap-2 max-w-[100%]'>
+        <span className='whitespace-nowrap'>Team Name:</span>{' '}
+        <span className='text-accent truncate w-full'>
+          <span className='truncate w-full'>
+            {team.team_name}
+          </span>
+        </span>
+      </div>
+      <div className='grid grid-cols-2 gap-2 w-full'>
+        {hasBeenRequested(team._id) ? (
+          <button className='btn' disabled>
+            Requested
+          </button>
+        ) : (
+          <button
+            className='btn btn-primary'
+            onClick={() => createRequest({ team_id: team._id })}
+          >
+            Request
+          </button>
+        )}
+        <button className='btn btn-info'>View Team</button>
+      </div>
     </li>
-  );
+  )
 
   if (user?.has_team) {
-    return null;
+    return null
   }
 
   return (
     <ModalWrapper>
       <button
-        className="btn btn-sm btn-circle btn-ghost absolute top-2 right-2 p-2"
+        className='btn btn-sm btn-circle btn-ghost absolute top-2 right-2 p-2 text-2xl'
         onClick={closeModal}
       >
         &times;
       </button>
-      <h3 className="font-bold text-xl mb-4">Available Teams</h3>
+      <h3 className='font-bold text-2xl pb-4 border-b border-slate-600 mb-4'>
+        Available Teams
+      </h3>
       {teams && user && requests ? (
-        <ul className="max-h-[50vh] overflow-y-auto">
+        <ul className='TEAMS_WRAP flex-col-tl gap-4 max-h-[50vh] overflow-y-auto'>
           {teams?.map(renderTeam)}
         </ul>
       ) : (
-        <div className="flex items-left justify-left w-full">
-          <div>Loading...</div>{" "}
+        <div className='flex items-left justify-left w-full'>
+          <div>Loading...</div>{' '}
         </div>
       )}
     </ModalWrapper>
-  );
-};
+  )
+}
