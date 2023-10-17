6b3f566ff697c4efcfc4b1bbfe2e29cac9a81c53
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-10-17 10:05:16 -0700

    Update: Add controller function to get user from session.

--- convex/suspects.ts
+++ convex/suspects.ts
@@ -1,5 +1,6 @@
 import { query } from "./_generated/server";
 import { v } from "convex/values";
+import { getUserFromAuthSession } from "./lib/getUserFromAuthSession";
 
 export const getAll = query(async ({ db }) => {
   return await db.query('suspects').collect()
@@ -12,3 +13,23 @@ export const getBySuspectId = query({
   handler: async ({ db }, { suspect_id }) => {
   return await db.get(suspect_id)
 }})
+
+
+export const getFromUserSession = query({
+  handler: async (ctx) => {
+    const { db } = ctx;
+
+    // get user.
+    const user = await getUserFromAuthSession(ctx);
+
+    // get suspect character information.
+    const suspectCard = await db
+      .query("suspects")
+      .filter((suspect) =>
+        suspect.eq(suspect.field("user_id"), user?._id),
+      )
+      .unique();
+
+    return suspectCard;
+  }
+})
