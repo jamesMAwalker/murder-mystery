f1eab2efe95794df114b4c377953efa1b1c64b29
Joshua <joshsmayhew@gmail.com>
2023-10-18 17:31:44 +0700

    add tabs for more compartmentalized display in user profile page, add visual effects and style to notifications tab if new notifications are present and add check for isSuspect to render button directing to suspect dashboard page

--- app/user-profile/page.tsx
+++ app/user-profile/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import { api } from "@/convex/_generated/api";
+import { cn } from "@/lib/utils";
 import { useConvexAuth, useMutation, useQuery } from "convex/react-internal";
 import { useAuth, useSession } from "@clerk/nextjs";
 import { usePrevious } from "../(hooks)/utility/usePrevious";
@@ -20,6 +21,17 @@ const UserProfilePage = () => {
   const { isSignedIn, isLoaded } = useSession();
   const user = useQuery(api.users.getFromSession);
   const { showModal } = useModal();
+  const [activeTab, setActiveTab] = useState(0);
+  const hasInvites = useQuery(api.invitations.getFromSessionByUser)
+    ?.length as number;
+  console.log("hasInvites: ", hasInvites);
+  const hasRequests = useQuery(api.requests.getFromSessionByTeam)
+    ?.length as number;
+
+  console.log("hasRequests: ", hasRequests);
+
+  const hasNotifications = hasInvites + hasRequests > 0;
+  console.log("hasNotifications: ", hasNotifications);
 
   // enum ModalType {
   //   JOIN = "join",
@@ -48,15 +60,28 @@ const UserProfilePage = () => {
   return (
     <div className="my-4 md:my-8 space-y-4 md:space-y-6 bg-slate-800 rounded-lg shadow-md p-2 md:p-4">
       <UserInfoSection />
-      {user.has_team && <TeamInfo />}
-      <TeamSelection
-        showJoinModal={() => showModal(ModalType.JOIN)}
-        showCreateModal={() => showModal(ModalType.CREATE)}
-        showAddModal={() => showModal(ModalType.ADD)}
-        showLeaveModal={() => showModal(ModalType.LEAVE)}
+      <SuspectsDashboardButton />
+      <ProfileTabs
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+        hasNotifications={hasNotifications}
       />
 
-      <NotificationsSection />
+      {activeTab === 0 && (
+        <>
+          {user?.has_team && <TeamInfo />}
+          <TeamSelection
+            showJoinModal={() => showModal(ModalType.JOIN)}
+            showCreateModal={() => showModal(ModalType.CREATE)}
+            showAddModal={() => showModal(ModalType.ADD)}
+            showLeaveModal={() => showModal(ModalType.LEAVE)}
+          />
+        </>
+      )}
+      {activeTab === 1 && <NotificationsSection />}
+
+      {/*
+      // <NotificationsSection /> */}
     </div>
   );
 };
@@ -70,6 +95,76 @@ function UserInfoSection() {
       <h1 className="text-xl md:text-2xl font-bold">
         Welcome, <span className="text-accent">{user?.name}</span>
       </h1>
+    </div>
+  );
+}
+
+function SuspectsDashboardButton() {
+  const isSuspect = true;
+
+  if (!isSuspect) {
+    return null;
+  }
+
+  return (
+    <button
+      className="btn btn-primary btn-lg w-full"
+      onClick={() => (window.location.href = "/suspect-dashboard")}
+    >
+      Suspects Dashboard
+    </button>
+  );
+}
+
+function ProfileTabs({ activeTab, setActiveTab, hasNotifications }: any) {
+  const hasTeam = useQuery(api.users.getFromSession)?.has_team;
+
+  // Base styles for tabs
+  const baseTabStyles =
+    "tab tab-lg flex-1 text-center cursor-pointer px-2 py-1 w-1/2 truncate";
+
+  return (
+    <div className="flex w-full tabs tabs-boxed">
+      {hasTeam ? (
+        <a
+          onClick={() => setActiveTab(0)}
+          className={cn(
+            baseTabStyles,
+            activeTab === 0 && "tab-active text-accent"
+          )}
+        >
+          My Team
+        </a>
+      ) : (
+        <a
+          onClick={() => setActiveTab(0)}
+          className={cn(
+            baseTabStyles,
+            activeTab === 0 && "tab-active text-accent"
+          )}
+        >
+          Get Started
+        </a>
+      )}
+      <a
+        onClick={() => setActiveTab(1)}
+        className={cn(
+          baseTabStyles,
+          "bg-blue-400 text-white shadow-md", // Color alteration and shadow for distinction
+          activeTab === 1 && "tab-active",
+          hasNotifications && "animate-pulse" // Animation for attention
+        )}
+      >
+        <i className="fas fa-bell"></i> {/* Iconography */}
+        <span className="ml-1">
+          {hasNotifications ? "New Notifications" : "Notifications"}
+        </span>
+        {hasNotifications && (
+          <span className="ml-2 rounded-full bg-red-500 text-white px-2 py-0.5 text-xs animate-bounce">
+            {hasNotifications}
+          </span>
+        )}
+      </a>
     </div>
   );
 }
