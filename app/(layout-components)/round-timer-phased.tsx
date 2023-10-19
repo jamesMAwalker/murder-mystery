6ba83853afb45292f6ca8767ef7ee24580b40dc8
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-10-19 03:16:27 -0700

    Update: Show timer only when there is time.

--- app/(layout-components)/round-timer-phased.tsx
+++ app/(layout-components)/round-timer-phased.tsx
@@ -7,14 +7,19 @@ import { api } from '@/convex/_generated/api'
 import { cn } from '@/lib/utils'
 import { DigitalTimer } from '../(components)/ui-components/digital-timer'
 
+/*
+  TODO: Refactor Rounds/Phases Into Game Object
+  # Currently, rounds are their own database table. They to reduce the number of moving parts, we should instead hard code the rounds into the game, and then have the game object store the current round information.
+*/
+
 export const PhasedRoundTimer = () => {
   const currentRound = useQuery(api.phased_rounds.getActiveRound)
   const currentPhase = useQuery(api.phased_rounds.getActivePhase)
+  // console.log('currentPhase: ', currentPhase);
 
   const [bgColor, setBgColor] = useState('bg-success')
   useEffect(() => {
     const remainingTime = currentPhase?.phase_current_time ?? 600
-
     if (remainingTime >= 61) setBgColor('bg-success')
     if (remainingTime <= 60) setBgColor('bg-warning')
     if (remainingTime <= 30) setBgColor('bg-error')
@@ -33,13 +38,14 @@ export const PhasedRoundTimer = () => {
       )}
     >
       <span>Round - {currentRound?.round_number}</span>
-      <span>{currentPhase?.phase_title}</span>
-      <DigitalTimer
-        totalSeconds={
-          currentPhase?.completed ? null : currentPhase?.phase_current_time!
-        }
-        style='minimal'
-      />
+      {currentPhase?.phase_current_time ? (
+        <>
+          <span>{currentPhase?.phase_title}</span>
+          <DigitalTimer style='minimal' />
+        </>
+      ) : (
+        <span>--:--</span>
+      )}
     </div>
   )
 }
