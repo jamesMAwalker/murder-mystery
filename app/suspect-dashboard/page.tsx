d638d107abbf0ef4aee7a73a622a5ec3a985c858
Joshua <joshsmayhew@gmail.com>
2023-10-20 15:35:20 +0700

    clean up suspect dashboard code

--- app/suspect-dashboard/page.tsx
+++ app/suspect-dashboard/page.tsx
@@ -1,18 +1,15 @@
 "use client";
 
-import { api } from "@/convex/_generated/api";
-import { cn } from "@/lib/utils";
-import { useQuery } from "convex/react";
 import { useEffect, useState } from "react";
+import { api } from "@/convex/_generated/api";
+import { useQuery } from "convex/react";
+import { cn } from "@/lib/utils";
 
 const SuspectDashboard = () => {
   const [activeTab, setActiveTab] = useState(0);
-  const [userIsNotSuspect, setUserIsNotSuspect] = useState(false);
 
   const suspectCard = useQuery(api.suspects.getFromUserSession);
-
   const user = useQuery(api.users.getFromSession);
-  const activeRound = useQuery(api.rounds.getActive) ?? 0;
 
   useEffect(() => {
     if (user && !suspectCard) {
@@ -21,94 +18,86 @@ const SuspectDashboard = () => {
   }, [user, suspectCard]);
 
   if (!user || !suspectCard) {
-    return (
-      <div className="flex-center h-40">
-        <span className="loading loading-ring loading-lg scale-150"></span>
-      </div>
-    );
+    return <LoadingIndicator />;
   }
 
   return (
     <div className="flex-col-tl gap-8">
-      <div className="INTRO flex-col-tl gap-8 w-full">
-        <h1 className="text-3xl font-bold">Suspect Dashboard</h1>
-        <div className="USER_INFO flex-col-tl gap-2">
-          <p className="text-2xl font-bold flex items-end gap-2">
-            <span>Welcome,</span>
-            <span className="text-secondary">{user?.name}!</span>
-          </p>
-        </div>
-        <div className="tabs tabs-boxed w-full">
-          {["Suspect", "Rd. 1", "Rd. 2", "Rd. 3"].map((tab, idx) => {
-            const isActive = idx === activeTab;
-
-            return (
-              <a
-                onClick={() => setActiveTab(idx)}
-                className={cn(
-                  "tab tab-lg",
-                  isActive && "tab-active text-accent"
-                )}
-                key={tab}
-              >
-                {tab}
-              </a>
-            );
-          })}
-        </div>
-      </div>
+      <DashboardHeader
+        user={user}
+        setActiveTab={setActiveTab}
+        activeTab={activeTab}
+      />
       {activeTab === 0 && <SuspectInfo suspectCard={suspectCard} />}
-      {activeTab === 1 && <InstructionsDisplay instructionsSet={0} />}
-      {activeTab === 2 && <InstructionsDisplay instructionsSet={1} />}
-      {activeTab === 3 && <InstructionsDisplay instructionsSet={2} />}
+      {[1, 2, 3].includes(activeTab) && (
+        <InstructionsDisplay instructionsSet={activeTab - 1} />
+      )}
     </div>
   );
 };
 
-export default SuspectDashboard;
+const LoadingIndicator = () => (
+  <div className="flex-center h-40">
+    <span className="loading loading-ring loading-lg scale-150"></span>
+  </div>
+);
 
-function InstructionsDisplay({ instructionsSet }: { instructionsSet: number }) {
+const DashboardHeader = ({ user, setActiveTab, activeTab }) => (
+  <div className="INTRO flex-col-tl gap-8 w-full">
+    <h1 className="text-3xl font-bold">Suspect Dashboard</h1>
+    <p className="text-2xl font-bold flex items-end gap-2">
+      <span>Welcome,</span>
+      <span className="text-secondary">{user?.name}!</span>
+    </p>
+    <div className="tabs tabs-boxed w-full">
+      {["Suspect", "Rd. 1", "Rd. 2", "Rd. 3"].map((tab, idx) => (
+        <a
+          key={tab}
+          onClick={() => setActiveTab(idx)}
+          className={cn(
+            "tab tab-lg",
+            idx === activeTab && "tab-active text-accent"
+          )}
+        >
+          {tab}
+        </a>
+      ))}
+    </div>
+  </div>
+);
+
+const InstructionsDisplay = ({ instructionsSet }) => {
   const instructionsData = useQuery(
     api.suspect_instructions.getFromUserSession
   );
-
-  // TODO: Refactor these conditionals.
-
   const instructions = instructionsData?.instructions;
+  const rdInstructions = instructions?.[instructionsSet];
 
-  if (!instructions) return null;
-
-  const rdInstructions =
-    instructions[instructionsSet as keyof typeof instructions]!;
-
-  return (
+  return rdInstructions ? (
     <div className="INSTRUCTIONS_DISPLAY flex-col-tl gap-4 full">
       <h4 className="text-xl font-bold">
-        {" "}
-        Round - {rdInstructions?.round_number}: Instructions
+        Round - {rdInstructions.round_number}: Instructions
       </h4>
     </div>
-  );
-}
+  ) : null;
+};
 
-function SuspectInfo({ suspectCard }: any) {
-  const [bioExpanded, setBioExpanded] = useState(false);
-
-  return (
-    <div className="USER_INFO flex-col-tl gap-4">
-      <h2 className="card-title flex gap-2">
-        <span>Your Character is:</span>
-        <span className="text-error">{suspectCard?.suspect_name}</span>
-      </h2>
-      <div
-        className={cn(
-          "badge badge-outline p-2",
-          suspectCard?.is_guilty ? "badge-error" : "badge-success"
-        )}
-      >
-        {suspectCard?.is_guilty ? "Guilty" : "Not Guilty"}
-      </div>
-      <p className="font-normal">{suspectCard?.bio}</p>
+const SuspectInfo = ({ suspectCard }) => (
+  <div className="USER_INFO flex-col-tl gap-4">
+    <h2 className="card-title flex gap-2">
+      <span>Your Character is:</span>
+      <span className="text-error">{suspectCard?.suspect_name}</span>
+    </h2>
+    <div
+      className={cn(
+        "badge badge-outline p-2",
+        suspectCard?.is_guilty ? "badge-error" : "badge-success"
+      )}
+    >
+      {suspectCard?.is_guilty ? "Guilty" : "Not Guilty"}
     </div>
-  );
-}
+    <p className="font-normal">{suspectCard?.bio}</p>
+  </div>
+);
+
+export default SuspectDashboard;
