c3acc63be5ee1e57dbd07c487dbab3c2acbf50d2
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-10-20 22:38:32 -0700

    Update: Add keys to nav options, hide guess before rd 1.

--- app/(layout-components)/bottom-nav.tsx
+++ app/(layout-components)/bottom-nav.tsx
@@ -20,6 +20,7 @@ import { useQuery } from 'convex/react'
 import { api } from '@/convex/_generated/api'
 
 interface IDropdownContent {
+  key: string
   page: string
   path: string
   icon: JSX.Element
@@ -27,17 +28,20 @@ interface IDropdownContent {
 }
 
 const pages: IDropdownContent[] = [
-  { page: 'Home', path: '/', icon: <HomeIcon /> },
-  { page: 'Story', path: '/background', icon: <StoryIcon /> },
-  { page: 'How-to', path: '/instructions', icon: <HowToIcon /> },
-  { page: 'Notes', path: '/notes', icon: <NotesIcon /> },
-  { page: 'Suspects', path: '/suspects', icon: <SuspectIcon /> },
-  { page: 'News', path: '/reports', icon: <AlertIcon /> },
-  { page: 'Profile', path: '/user-profile', icon: <ProfileIcon /> },
-  { page: 'Guess! ', path: '/submit-guess', icon: <GuessIcon /> }
+  { key: 'home', page: 'Home', path: '/', icon: <HomeIcon /> },
+  { key: 'story', page: 'Story', path: '/background', icon: <StoryIcon /> },
+  { key: 'howto', page: 'How-to', path: '/instructions', icon: <HowToIcon /> },
+  { key: 'notes', page: 'Notes', path: '/notes', icon: <NotesIcon /> },
+  { key: 'suspects', page: 'Suspects', path: '/suspects', icon: <SuspectIcon /> },
+  { key: 'news', page: 'News', path: '/reports', icon: <AlertIcon /> },
+  { key: 'profile', page: 'Profile', path: '/user-profile', icon: <ProfileIcon /> },
+  { key: 'guess', page: 'Guess! ', path: '/submit-guess', icon: <GuessIcon /> },
+  { key: 'admin', page: 'Admin', path: '/admin', icon: <GuessIcon /> },
+  { key: 'script', page: 'Script', path: '/suspect-dashboard', icon: <GuessIcon /> }
 ]
 
 export const BottomNav = () => {
+  const userRole: string = 'Admin'
   const activeRound = useQuery(api.phased_rounds.getActiveRound)
   console.log('activeRound: ', activeRound)
 
@@ -86,21 +90,31 @@ export const BottomNav = () => {
       </p>
 
       <ul className='w-full px-8 collapse-content grid grid-cols-4 gap-1 p-0'>
-        {pages.map(({ page, path, icon }, idx) => {
+        {pages.map(({ key, page, path, icon }, idx) => {
           const active = idx === current
-          
+
           // hide suspects until round 1.
           const hidden =
             activeRound?.round_number! <= 0 && path === '/suspects'
               ? true
-              : false 
+              : false
+
+          const shouldBeHidden = {
+            guess: activeRound?.round_number! <= 0,
+            suspects: activeRound?.round_number! <= 0,
+            admin: userRole.toLowerCase() !== 'admin',
+            suspect: userRole.toLowerCase() !== 'suspect'
+          }
+
+          const hide = shouldBeHidden[key as keyof typeof shouldBeHidden]
+          console.log('hide: ', hide);
 
           return (
             <li
               className={cn(
                 'bg-white/5 hover:bg-secondary/30 text-white hover:text-secondary gap-2 aspect-square flex-col-center w-full text-primary-content peer-checked:text-secondary-content focus:text-secondary',
-                hidden && '!hidden'
-              )}
+                hide && '!hidden'
+                )}
               onClick={() => handleClick(idx, path)}
               key={page}
             >
