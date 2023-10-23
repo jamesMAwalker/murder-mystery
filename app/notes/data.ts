5975ae3b61286e2253eacc8712014055adb29738
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-10-23 02:26:25 -0700

    Add: General notes set up files.

---
+++ app/notes/data.ts
@@ -0,0 +1,7 @@
+import { IGeneralNote } from "./types";
+
+export const GENERAL_NOTE: IGeneralNote = {
+  _id: '',
+  suspect_name: 'Any!',
+  image_url: 'https://res.cloudinary.com/jameswalker-work/image/upload/v1698053088/murder-mystery/c41bee3acd912a2796fa26582fb075a4_chsbdl.jpg',
+}
---
+++ app/notes/types.ts
@@ -0,0 +1,5 @@
+export interface IGeneralNote {
+  _id: string;
+  suspect_name: string;
+  image_url: string;
+}
