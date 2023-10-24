2db1e1c30c7952b2610054e8ced709a36dda8033
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-10-24 10:56:50 -0700

    Update: Language change.

--- app/notes/data.ts
+++ app/notes/data.ts
@@ -2,6 +2,6 @@ import { IGeneralNote } from "./types";
 
 export const GENERAL_NOTE: IGeneralNote = {
   _id: '',
-  suspect_name: 'Any!',
+  suspect_name: 'General',
   image_url: 'https://res.cloudinary.com/jameswalker-work/image/upload/v1698053088/murder-mystery/c41bee3acd912a2796fa26582fb075a4_chsbdl.jpg',
 }
