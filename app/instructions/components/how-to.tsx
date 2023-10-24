118d4b7d41ab5af3fc5e3ae0d4ce26994c4bf45d
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-10-24 12:17:18 -0700

    Add: How-to page component.

---
+++ app/instructions/components/how-to.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+export const HowToPage = () => {
+  return (
+    <>
+      <div className='flex-col-tl '>
+        <h3 className='text-xl font-bold text-primary'>Overview</h3>
+        <p>
+          Get ready for an engaging and immersive way to enhance your English
+          speaking and comprehension skills with our Murder Mystery English
+          Game! To kick things off, assemble your teams, with a minimum of two
+          players per team. Then, brace yourselves for three thrilling timed
+          rounds.
+        </p>
+      </div>
+      <div className='flex-col-tl '>
+        <h3 className='text-lg font-bold text-accent'>The Rounds</h3>
+        <p>
+          You will participate in three rounds, each involving suspect
+          interviews, note collection, and discussions with your teammates. As
+          the rounds progress, new evidence from the investigation will be
+          introduced. Stay prepared to integrate this new information into your
+          evolving theories!
+        </p>
+      </div>
+      <div className='flex-col-tl '>
+        <h5 className='text-lg italic text-accent'>Interview Phase</h5>
+        <p>
+          In the first phase, every teammate gets the opportunity to interview
+          one of the six suspects individually. You must carefully record your
+          interview notes during this crucial step.
+        </p>
+      </div>
+      <div className='flex-col-tl '>
+        <h5 className='text-lg italic text-accent'>New Evidence!</h5>
+        <p>
+          As the interview phase concludes, a new piece of evidence will come
+          into play. This added clue sets the stage for the next part of the
+          round.
+        </p>
+      </div>
+      <div className='flex-col-tl '>
+        <h5 className='text-lg italic text-accent'>Discussion Phase</h5>
+        <p>
+          During the discussion phase, teammates work together to analyze the
+          fresh evidence, combining it with their interview findings. This
+          collaborative effort involves comparing notes and crafting hypotheses
+          about the identity of the culprit. It&#39;s also the perfect moment to
+          strategize the next suspect to interview and brainstorm the most
+          relevant questions to ask them. Your teamwork and language skills will
+          be put to the test in this engaging phase of the game.
+        </p>
+        <h5>Conclusion</h5>
+      </div>
+      <div className='flex-col-tl '>
+        <h5 className='text-lg font-bold text-secondary'>Conclusion</h5>
+        <p>
+          After three rounds of investigative fun, your team will submit its
+          final verdict on the guilty suspect. Get it right, and you&#39;ll earn
+          a fantastic prize: free English lessons! This game promises a
+          thrilling journey of language improvement while unraveling a
+          captivating murder mystery.
+        </p>
+      </div>
+    </>
+  )
+}
