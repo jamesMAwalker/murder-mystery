4cac2eaf97c2db0a51a02c7684367ac2ee24d48d
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-10-25 07:46:15 -0700

    Update: Add custom styling prop.

--- app/(components)/ui-components/loaders.tsx
+++ app/(components)/ui-components/loaders.tsx
@@ -1,14 +1,21 @@
-export const SectionLoader = () => {
+import { cn } from '@/lib/utils'
+
+export const SectionLoader = ({ classes }: { classes?: string }) => {
   return (
-    <div className='flex justify-start animate-pulse full bg-slate-400/10 rounded-md p-4'>
+    <div
+      className={cn(
+        'flex justify-start animate-pulse full bg-slate-400/10 rounded-md p-4',
+        classes
+      )}
+    >
       <span className='loading loading-ring loading-lg scale-50 origin-left' />
     </div>
   )
 }
 
-export const SingleLoader = () => {
+export const SingleLoader = ({ classes }: { classes?: string }) => {
   return (
-    <div className='h-auto w-auto'>
+    <div className={cn('h-auto w-auto', classes)}>
       <span className='loading loading-ring loading-lg scale-50' />
     </div>
   )
